8a4aa3e6d9ac2262f6556ffd5998725ffd53aeef
chao.li <chao.li@team-lab.cn>
2021-05-19 16:11:52 +0800

    Initial Commit

---
+++ Custom_KVC/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Custom_KVC
+//
+//  Created by lab team on 2021/5/19.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
